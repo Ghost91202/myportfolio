3620f724b0686820f3df4c308d8f57cd19e9a44a
karan sharma <127385706+Ghost91202@users.noreply.github.com>
2023-09-09 10:41:50 +0530

    10/9/2023

---
+++ portfolio/about/script.js
@@ -0,0 +1,191 @@
+// CURSOR
+var cursor = $(".cursor"),
+follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+
+    TweenMax.set(follower, {
+        css: {
+        left: posX - 12,
+        top: posY - 12
+        }
+    });
+
+    TweenMax.set(cursor, {
+        css: {
+        left: mouseX,
+        top: mouseY
+        }
+    });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+// yellow circle
+$(".link").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$(".link").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
+
+
+/*
+https://youtu.be/zwl3kZPZ8H8
+*/
+
+const text = document.querySelector(".text");
+text.innerHTML = text.innerText
+	.split("")
+	.map(
+		(char, i) => `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`
+	)
+	.join("");
+
+
+    gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const sections = gsap.utils.toArray('section');
+
+    let scrollTween = gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.wrapper',
+            pin: true,
+            scrub: 0.5,
+            snap: 1 / (sections.length - 1),
+            start: 'top top',
+            end: 3000,
+        }
+    });
+    
+    gsap.to('.logo3', {
+        fontSize: '10.5rem',
+        top: '0.1rem',
+        opacity:"0",
+        scrollTrigger: {
+            trigger: '.logo3',
+            start: 'center top',
+            end: 1500,
+            scrub: 3,
+        }
+    });
+
+    gsap.to('.line', {
+        height: '10rem',
+        scrollTrigger: {
+            trigger: '.line',
+            scrub: 0.5,
+            start: 'center center',
+            end: 2000,
+        }
+    },"atm");
+    gsap.to('#anim', {
+        fontSize: '60.5rem',
+        top: '50%',
+        color:"black",
+        opacity:"0.7",
+        scrollTrigger: {
+            trigger: '#anim',
+            start: " center 3%",
+            end: "center 30%",
+            scrub: 5,
+            z:100,
+        }
+    },"atm");
+
+    document.querySelectorAll('.character').forEach(el => {
+
+        gsap.to(el.querySelector('.caption'), {
+            x: 0,
+            y: 0,
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('.caption'),
+                start: 'top bottom',
+                end: '+=1000',
+                scrub: 0.5,
+            }
+        });
+
+        gsap.to(el.querySelector('.quote'), {
+            y: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('.quote'),
+                start: 'top bottom',
+                end: '+=20%',
+                scrub: 0.5,
+            }
+        });
+
+        gsap.to(el.querySelector('.nickname'), {
+            y: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('.nickname'),
+                start: 'top bottom',
+                end: '+=10%',
+                scrub: 0.5,
+            }
+        });
+
+        gsap.to(el.querySelector('.block'), {
+            x: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('.block'),
+                start: 'top bottom',
+                end: '+=' + window.innerWidth,
+                scrub: 0.5,
+            }
+        });
+
+        gsap.to(el.querySelector('img'), {
+            y: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('img'),
+                start: 'top bottom',
+                end: '+=50%',
+                scrub: 0.5,
+            }
+        });
+
+        gsap.to(el.querySelector('.huge-text'), {
+            y: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: el.querySelector('.huge-text'),
+                start: 'top bottom',
+                end: '+=100%',
+                scrub: 0.5,
+            }
+        });
+
+    });
+
+});
